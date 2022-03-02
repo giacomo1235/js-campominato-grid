73cc2e9044521a890b2ff935bdd177504381c4bf
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-02 18:04:52 +0100

    indentato meglio

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ let areaNumbers = document.querySelector('.area-numbers');
 
 function easyLevel(){
     for (let i = 1; i <= 100; i++) {
-        areaNumbers.innerHTML += `<div class= "box">${i}</div>`
+        areaNumbers.innerHTML += `<div class= "box">${i}</div>`;
     };
 };
 
 
 function mediumLevel(){
     for (let i = 1; i <= 81; i++) {
-        areaNumbers.innerHTML += `<div class= "box">${i}</div>`
+        areaNumbers.innerHTML += `<div class= "box">${i}</div>`;
     };
 }
 
 function hardLevel(){
     for (let i = 1; i <= 49; i++) {
-        areaNumbers.innerHTML += `<div class= "box">${i}</div>`
+        areaNumbers.innerHTML += `<div class= "box">${i}</div>`;
     };
 };
 
@@ -25,7 +25,10 @@ function changeLevel() {
     var select = document.getElementById('change');
 	var option = select.options[select.selectedIndex].value;
 
-    if (option == 1) {
+   if (option == 0){
+        areaNumbers.innerHTML = '';
+   }
+    else if (option == 1) {
         areaNumbers.innerHTML = '';
         easyLevel();
    }  else if (option == 2) {
