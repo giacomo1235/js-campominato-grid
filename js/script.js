b370fca6d88b628e9dae3155aa3c9371bc9b1ddc
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-02 17:07:34 +0100

    proovato a collegare form

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function easyLevel(){
     };
 };
 
+
 function mediumLevel(){
     for (let i = 1; i <= 81; i++) {
    
@@ -21,5 +22,25 @@ function hardLevel(){
     };
 }
 
+const btnPlay = document.getElementById('play')
+const eleLevel = parseInt(document.querySelectorAll('option'));
+
+btnPlay.addEventListener('click', play);
+
+function play() {
+    if (eleLevel == 1) {
+         easyLevel();
+    }  else if (eleLevel == 2) {
+        mediumLevel();
+    } else {
+        hardLevel()
+    }
+  // if value = 1 restituisci la function easylevel 
+  // if value = 2 restituisci la function mediumlevel
+  // if value = 3 restituisci la function hardlevel
+}
+
+
+ 
 
 
