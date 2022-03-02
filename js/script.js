a34e09eefb9d88c49be2f5f4e8b14fa66aa4bec5
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-02 17:57:31 +0100

    funziona

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let areaNumbers = document.querySelector('.area-numbers');
 
 function easyLevel(){
     for (let i = 1; i <= 100; i++) {
-   
         areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     };
 };
@@ -10,46 +9,33 @@ function easyLevel(){
 
 function mediumLevel(){
     for (let i = 1; i <= 81; i++) {
-   
         areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     };
 }
 
 function hardLevel(){
     for (let i = 1; i <= 49; i++) {
-   
         areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     };
-}
+};
 
-/* const btnPlay = document.getElementById('play');
-const eleLevel = document.querySelectorAll('option');
-
-btnPlay.addEventListener('click', play);
-
-function play() {
-    const eleLevel = document.querySelectorAll('option').value;
-    if (eleLevel == 1) {
-         easyLevel();
-    }  else if (eleLevel == 2) {
-        mediumLevel();
-    } else {
-        hardLevel()
-    }
-} */
 
 function changeLevel() {
+
     var select = document.getElementById('change');
 	var option = select.options[select.selectedIndex].value;
-    console.log(option)
+
     if (option == 1) {
+        areaNumbers.innerHTML = '';
         easyLevel();
    }  else if (option == 2) {
+    areaNumbers.innerHTML = '';
        mediumLevel();
    } else {
-       hardLevel()
+       areaNumbers.innerHTML = '';
+       hardLevel();
    }
-}
+};
 
 
  
